64d9cc824a448e0d63abe54b050905e8d6d6ba31
solji <solji.choi89@gmail.com>
2024-12-10 12:33:54 +0900

    getAgeAndUp

--- src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // @Controller 어노테이션을 붙여주면 스프링부트가 이 클래스를 컨트롤러로 인식하게 된다.
 @Controller
 public class HomeController {
+    private int age = 22;
+
     @GetMapping("/") // @GetMapping("/") 어노테이션을 붙여주면 이 메소드는 "/" 경로로 들어왔을 때 실행된다.
     @ResponseBody // @ResponseBody 어노테이션을 붙여주면 리턴값이 뷰 파일명이 아니라 응답 바디에 들어가게 된다.
     public String showMain() {
@@ -23,5 +25,11 @@ public class HomeController {
                 <h1>저는 홍길동입니다.</h1>
                 <p><strong>소개페이지</strong>입니다.</p>
                 """;
+    }
+
+    @GetMapping("/ageUp")
+    @ResponseBody
+    public int getAgeAndUp() {
+        return age++;
     }
 }
