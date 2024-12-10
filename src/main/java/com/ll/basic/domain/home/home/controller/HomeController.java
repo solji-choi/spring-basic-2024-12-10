1b587e1f5083d069eeef97f503b179950341334e
solji <solji.choi89@gmail.com>
2024-12-10 14:14:06 +0900

    getArticle

--- src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ll.basic.domain.home.home.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -95,4 +97,17 @@ public class HomeController {
         return Map.of("name", "Paul", "hobby", "reading");
     }
 
+    @GetMapping("/article")
+    @ResponseBody
+    public Article getArticle() {
+        return new Article(1, "제목", "내용");
+    }
+}
+
+@AllArgsConstructor
+@Getter
+class Article {
+    private long id;
+    private String title;
+    private String body;
 }
