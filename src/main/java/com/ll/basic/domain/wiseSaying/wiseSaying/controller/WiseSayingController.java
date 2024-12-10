9cc797efd05d5c6900316295c67390336f27e762
solji <solji.choi89@gmail.com>
2024-12-10 15:33:13 +0900

    @RequiredArgsConstructor

--- src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -2,6 +2,7 @@ package com.ll.basic.domain.wiseSaying.wiseSaying.controller;
 
 import com.ll.basic.domain.domain.wiseSaying.wiseSaying.entity.WiseSaying;
 import com.ll.basic.domain.domain.wiseSaying.wiseSaying.service.WiseSayingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,13 +10,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
-@ResponseBody //여기에 붙이면 모든 메소드에 적용됨
+@ResponseBody
+@RequiredArgsConstructor//여기에 붙이면 모든 메소드에 적용됨
 public class WiseSayingController {
     private final WiseSayingService wiseSayingService;
-
-    public WiseSayingController(WiseSayingService wiseSayingService) {
-        this.wiseSayingService = wiseSayingService;
-    }
 
     @GetMapping("/wiseSayings")
     public List<WiseSaying> getItems() {
