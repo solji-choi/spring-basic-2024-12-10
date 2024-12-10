74c4cdbd19f6ee0110fb370b2b6466fe50ca348c
solji <solji.choi89@gmail.com>
2024-12-10 15:31:30 +0900

    Service

--- src/main/java/com/ll/basic/domain/domain/wiseSaying/wiseSaying/service/WiseSayingService.java
+++ src/main/java/com/ll/basic/domain/domain/wiseSaying/wiseSaying/service/WiseSayingService.java
@@ -1,13 +1,13 @@
 package com.ll.basic.domain.domain.wiseSaying.wiseSaying.service;
 
 import com.ll.basic.domain.domain.wiseSaying.wiseSaying.entity.WiseSaying;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-@Component
+@Service
 public class WiseSayingService {
     private final List<WiseSaying> wiseSayings;
     private long lastId;
