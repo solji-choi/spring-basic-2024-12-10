da8fd2647ae214415c669829550b8338f692837f
solji <solji.choi89@gmail.com>
2024-12-10 15:34:12 +0900

    @RestController = Controller + Responsebody

--- src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -3,14 +3,12 @@ package com.ll.basic.domain.wiseSaying.wiseSaying.controller;
 import com.ll.basic.domain.domain.wiseSaying.wiseSaying.entity.WiseSaying;
 import com.ll.basic.domain.domain.wiseSaying.wiseSaying.service.WiseSayingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@ResponseBody
+@RestController
 @RequiredArgsConstructor//여기에 붙이면 모든 메소드에 적용됨
 public class WiseSayingController {
     private final WiseSayingService wiseSayingService;
