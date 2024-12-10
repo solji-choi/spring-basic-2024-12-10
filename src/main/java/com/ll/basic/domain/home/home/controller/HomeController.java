ae48ece0c9d94b893c1edd28b590847431810013
solji <solji.choi89@gmail.com>
2024-12-10 12:18:46 +0900

    setting

---
+++ src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.ll.basic.domain.home.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String showMain() {
+        return """
+                <h1>안녕하세요.</h1>
+                """;
+    }
+}
